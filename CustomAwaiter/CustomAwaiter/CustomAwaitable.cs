5c5be87c0c938882abaf80f3cd3d98e0c0107260
mc0re <mnikitin@hotmail.com>
2018-11-19 22:12:23 +0100

    Added custom awaiter example.

---
+++ CustomAwaiter/CustomAwaiter/CustomAwaitable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace CustomAwaiter
+{
+    public class CustomAwaitable
+    {
+        public CustomAwaiter GetAwaiter()
+        {
+            return new CustomAwaiter("Custom GetAwaiter");
+        }
+    }
+
+
+    public class CustomAwaiter : INotifyCompletion
+    {
+        #region Fields
+
+        private string mResult;
+
+        #endregion
+
+
+        #region Properties
+
+        public static bool RunAsync { get; set; } = false;
+
+
+        public bool IsCompleted
+        {
+            get
+            {
+                return !RunAsync;
+            }
+        }
+
+        #endregion
+
+
+        public CustomAwaiter(string result)
+        {
+            this.mResult = result;
+        }
+
+
+        void INotifyCompletion.OnCompleted(Action continuation)
+        {
+            var sc = SynchronizationContext.Current;
+            var t = new Timer(
+                s => sc.Post(s1 => continuation.Invoke(), s),
+                null, 1000, Timeout.Infinite);
+        }
+
+
+        public string GetResult()
+        {
+            return mResult;
+        }
+    }
+}
---
+++ CustomAwaiter/CustomAwaiter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CustomAwaiter
+{
+    class Program
+    {
+        private static AutoResetEvent mEvt = new AutoResetEvent(false);
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sync");
+            CustomAwaiter.RunAsync = false;
+            MainMethod();
+
+            Console.WriteLine("Async");
+            CustomAwaiter.RunAsync = true;
+            MainMethod();
+        }
+
+
+        private static void MainMethod()
+        {
+            Console.WriteLine("Start main method");
+            DoWork();
+            Console.WriteLine("DoWork returned");
+            mEvt.WaitOne();
+            Console.WriteLine("Event is triggered, main method exits");
+        }
+
+        private static async void DoWork()
+        {
+            var res = await DoWorkAsync();
+            Console.WriteLine($"DoWorkAsync returned {res}");
+            mEvt.Set();
+        }
+
+
+        private static CustomAwaitable DoWorkAsync()
+        {
+            return new CustomAwaitable();
+        }
+    }
+}
